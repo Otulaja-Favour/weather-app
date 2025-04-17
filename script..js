fd0605498e0cf18b80b2fcd9083e6b80ed881993
Otulaja Favour <otulajafavour14@gmail.com>
2025-04-17 17:04:27 +0100

    settleld the api aspect

--- script..js
+++ script..js
@@ -1,33 +1,27 @@
 async function getWeather(city) {
-  const API_KEY = "1862a4c96c034e74f156c00ddb278294";
-  try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-    );
-    const data = await response.json();
-
-    console.log(data);
-    
-document.getElementById('state').innerText = city
-document.getElementById('temperature').innerText =  data.main.temp, "°C"
-document.getElementById('situation').innerText = data.weather[0].description
-document.getElementById('humidity').innerText = data.main.humidity, "%"
-// document.getElementById('')
-// document.getElementById('')
-
-
-
-    console.log(`Weather in ${city}:`);
-    console.log("Temperature:", data.main.temp, "°C");
-    console.log("Description:", data.weather[0].description);
-    // console.log("Description:", data.weather[1].description);
-
-    console.log("Humidity:", data.main.humidity, "%");
-
-    return data;
-  } catch (error) {
-    console.error("Error fetching weather:", error);
-    return null;
+    const API_KEY = "1862a4c96c034e74f156c00ddb278294";
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+  
+     
+      const data = await response.json();
+  
+      document.getElementById('state').innerText = `City: ${city}`;
+      document.getElementById('temperature').innerText = `Temperature: ${data.main.temp} °C`;
+      document.getElementById('situation').innerText = `Weather: ${data.weather[0].description}`;
+      document.getElementById('humidity').innerText = `Humidity: ${data.main.humidity}%`;
+  
+    } catch (error) {
+      console.error("Error fetching weather:", error);
+    }
   }
-}
-getWeather('london')
+  
+  document.getElementById('searchBtn').addEventListener('click', () => {
+    const city = document.getElementById('city').value.trim();
+    if (city) {
+      getWeather(city);
+    }
+  });
+  
